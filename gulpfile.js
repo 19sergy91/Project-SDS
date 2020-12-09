58bb4f3b55a29b419813fdcc016c3171a3d4c02a
19sergy91 <73836016+19sergy91@users.noreply.github.com>
2020-12-09 19:55:09 +0300

    09.12.2020

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest, watch } = require('gulp');
+
+const scss = require('gulp-sass');
+
+function styles() {
+    return src('app/scss/style.scss')
+        .pipe(scss({ outputStyle: 'expanded' }))
+        .pipe(dest('app/css'))
+}
+
+function watching() {
+    watch(['app/scss/**/*.scss'], styles)
+}
+
+exports.styles = styles;
+exports.watching = watching;
