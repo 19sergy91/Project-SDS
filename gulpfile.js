edda17adf1dbba20333edaf4edaad494f5f89467
19sergy91 <73836016+19sergy91@users.noreply.github.com>
2021-01-15 18:44:00 +0300

    12.12.2020

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,32 @@
-const { src, dest, watch } = require('gulp');
-
+const { src, dest, watch, parallel } = require('gulp');
 const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: 'dist/'
+        }
+    })
+}
+
 
 function styles() {
-    return src('app/scss/style.scss')
+    return src('src/scss/style.scss')
         .pipe(scss({ outputStyle: 'expanded' }))
-        .pipe(dest('app/css'))
+        .pipe(concat('style.min.css'))
+        .pipe(dest('dist/css'))
+        .pipe(browserSync.stream())
 }
 
 function watching() {
-    watch(['app/scss/**/*.scss'], styles)
+    watch(['src/scss/**/*.scss'], styles)
+    watch(['dist/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
 exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.default = parallel(styles, watching, browsersync);
